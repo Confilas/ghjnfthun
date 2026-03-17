c3e95b186edc95836e862ad89f716e6a37487241
Confilas <kobz.dima@mail.ru>
2026-03-17 06:32:33 +0300

    Update 67.lua

--- 67.lua
+++ 67.lua
@@ -12,7 +12,7 @@ local localPlayer = Players.LocalPlayer
 local WEBHOOK_1_25M   = "https://discord.com/api/webhooks/1454372752034365566/EmLdd5U_wxp6Ziy8gAAiGB7MHpeWPaDBZzS8vfAQSD2dWsB0ZyPAEfDYK0n869ObblnT"
 local WEBHOOK_26_100M = "https://discord.com/api/webhooks/1456399330662682714/1HTEJNBTO1qhXMex8-5YBB1_Q5ekmyHtgvUJ886zitqpl6IC_aXhjKb5QgpjXns1s4uh"
 local WEBHOOK_101M    = "https://discord.com/api/webhooks/1456399389336801463/kyZ8kGtNnSL3Ee1YJteiCWdKWA_KlVFdUNEVAAWr9_kkwGJc6r-3Xg7D8w56aUxRfa1q"
-local WEBHOOK_SPECIAL = "https://discord.com/api/webhooks/1483297552794255522/uHgMWzVX8-3gSoPmao4UZNgTy95ofZy-33zfr5xjP3npQ31MGj4kFwSsKsumKJy5Kp-5"
+local WEBHOOK_SPECIAL = "https://discord.com/api/webhooks/1483306980373430492/HRh-YS-WmEkgKJwAsLlnNEdxDslb0v9tGN-Rl7uSigE0TKG0YRySYc8pIkTi3Csk2SU0"
 
 local GAME_ID    = 109983668079237
 local SERVER_URL = "https://raw.githubusercontent.com/Confilas/ghjnfthun/refs/heads/main/fghs"
